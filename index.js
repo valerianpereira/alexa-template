731a45b42d1d412ec1e53e461046b978da3b8b6e
Valerian Pereira <valerianpereira25@gmail.com>
2017-12-24 00:41:45 +0530

    Basic code piece

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+
+//Variables
+var Alexa = require('alexa-sdk');
+var SKILL_NAME = "Alexa Template";
+var APP_ID = "";
+
+//List of compliments
+var COMPLIMENT_LIST = [
+    "Damn son, you're looking mighty fine today!",
+    "Wow! You made an Alexa skill. You're smarter than I thought!",
+    "If you were a food, you'd be an endless supply of CheeseCake"
+];
+
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+} 
+
+var handlers = {
+    'LaunchRequest': function(){
+        this.emit('GetCompliment');
+    },
+    'GetComplimentIntent': function() {
+        this.emit('GetCompliment');
+    },
+    'GetCompliment': function(){
+        var complimentIndex = Math.floor(Math.random() * COMPLIMENT_LIST.length);
+        var randomCompliment = COMPLIMENT_LIST[complimentIndex];
+
+        //Output
+        var speechOutput = "Your compliment " + randomCompliment;
+        this.emit(":tellWithCard", speechOutput, SKILL_NAME, randomCompliment);
+
+    },
+    'AMAZON.HelpIntent': function(){
+        var speechOutput = "You can say give me a compliment or you can say exit..";
+        var reprompt = "What can I help you with ?";
+        this.emit(":ask", speechOutput, reprompt);
+    },
+    'AMAZON.StopIntent': function(){
+        this.emit(":tell", "Good Bye!");
+    },
+    'AMAZON.CancelIntent': function(){
+        this.emit(":tell", "Good Bye!");
+    }
+}
